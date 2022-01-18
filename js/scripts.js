bdb2bd304f5c33a50313b78dbe9f9369d1e9c39a
Jase Seeley <jcseeley31590@yahoo.com>
2022-01-18 14:39:10 -0800

    add required functionality.
    Co-Authored-by: Ryan Bass <theryanbass@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,35 @@
 $(document).ready(function() {
-
   $("form#grocery-list").submit(function(event) {
+    event.preventDefault();
     $("#groceries").toggle();
     $("#list").toggle();
-  
-    event.preventDefault();
-    const groceryList = [$("input#grocery1").val(), $("input#grocery2").val(), $("input#grocery3").val(), $("input#grocery4").val(), $("input#grocery5").val()];
+    let groceryList = [];
+    groceryOne = $("input#grocery1").val();
+    groceryTwo = $("input#grocery2").val();
+    groceryThree = $("input#grocery3").val();
+    groceryFour = $("input#grocery4").val();
+    groceryFive = $("input#grocery5").val();
+    if (groceryOne != "") {
+      groceryList.push(groceryOne);
+    }
+    if (groceryTwo != "") {
+      groceryList.push(groceryTwo);
+    }
+    if (groceryThree != "") {
+      groceryList.push(groceryThree)
+    }
+    if (groceryFour != "") {
+      groceryList.push(groceryFour)
+    }
+    if (groceryFive != "") {
+      groceryList.push(groceryFive)
+    }
     groceryList.sort();
-    
     groceryList.forEach(function(grocery) {
-      groceryArray = grocery.split("");
+      let groceryArray = grocery.split("");
       groceryArray[0] = groceryArray[0].toUpperCase();
       grocery = groceryArray.join("");
       $("#output").append("<li>" + grocery + "</li>");
-
-
     });
-
   });
 });
